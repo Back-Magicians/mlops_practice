eada8196ba13c31ece4394fb884d1a4a49a76ce2
VasyaRodionov1 <vasyarod@gmail.com>
2024-03-18 21:27:20 +0500

    add training RandomForest model

---
+++ lab2/model_training.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+import pickle
+
+# Загрузка данных
+X_train = pd.read_csv('data/X_train.csv')
+y_train = pd.read_csv('data/y_train.csv')
+
+# Преобразование текстовых признаков в числовые
+X_train = pd.get_dummies(X_train)
+
+# Обучение Random Forest Regressor
+model = RandomForestRegressor(n_estimators=100, random_state=42)
+model.fit(X_train, y_train.values.ravel())
+
+# Сохранение модели в файл pickle
+with open('model.pickle', 'wb') as f:
+    pickle.dump(model, f)
