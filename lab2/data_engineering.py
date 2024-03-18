d54c543dd9a3a27ae5435099842901d3d7bd3a2d
NiktiaGorbunov <grb.nikita@yandex.ru>
2024-03-19 00:33:26 +0500

    update engineering. Create ordinal_coding()

--- lab2/data_engineering.py
+++ lab2/data_engineering.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.impute import KNNImputer
-
+from sklearn.preprocessing import OrdinalEncoder
+import json
 
 def filling_nan_KNN_method(data):
     ds = data.copy()
@@ -26,5 +27,23 @@ def fill_columns_median_value(data):
     for column in data_nan_columns:
         most_frequent_value = data[column].mode()[0]
         data.fillna({column: most_frequent_value}, inplace=True)
+
+    return data
+
+def ordinal_coding(data):
+
+    ordinal_encoder = OrdinalEncoder()
+
+    columns_to_encode = data.select_dtypes(['object']).columns
+
+    data[columns_to_encode] = ordinal_encoder.fit_transform(data[columns_to_encode])
+
+    # Получение словаря с кодами
+    encoding_dict = {}
+    for i, col in enumerate(columns_to_encode):
+        encoding_dict[col] = {value: code for code, value in enumerate(ordinal_encoder.categories_[i])}
+
+    with open('encoding_dict.json', 'w') as json_file:
+        json.dump(encoding_dict, json_file)
 
     return data
