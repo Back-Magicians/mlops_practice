c5bdd328236ea54d1097a19ebbfe5ec6095bfe5c
vanek021 <vanekmenyakin@gmail.com>
2024-04-02 20:42:31 +0500

    add fastapi application

--- lab3/main.py
+++ lab3/main.py
@@ -1,23 +1,15 @@
+from fastapi import FastAPI, File
+from fastapi.responses import Response
 from virtual_background import process_image
-import cv2
-import numpy as np
+from typing import Annotated
 
-# тестовое изображение и фон
-IMAGE = 'datasets/obj2.jpg'
-BACKGROUND = 'background/back4.jpg'
+app = FastAPI()
 
-# преобразуем данные в байтовый формат
-# в таком виде получаем данные с вебки
-data_img = cv2.imread(IMAGE)
-data_img_bytes = cv2.imencode('.jpg', data_img)[1].tobytes()
 
-back_img = cv2.imread(BACKGROUND)
-back_img_bytes = cv2.imencode('.jpg', back_img)[1].tobytes()
-
-# преобразуем данные из байтов в обычные
-processed_img_bytes = process_image(data_img_bytes, back_img_bytes)
-nparr = np.frombuffer(processed_img_bytes, np.byte)
-processed_img = cv2.imdecode(nparr, cv2.IMREAD_ANYCOLOR)
-
-# локальное сохранение
-cv2.imwrite('processed_images/image.jpg', processed_img)
+@app.post("/file/process-files")
+def process_files(
+        image: Annotated[bytes, File()],
+        background: Annotated[bytes, File()]
+):
+    result = process_image(image, background)
+    return Response(content=result, media_type="image/png")
