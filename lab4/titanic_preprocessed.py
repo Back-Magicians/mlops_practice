caa47b4347448a5f6c0044203fb211c4b83d5c51
VasyaRodionov1 <vasyarod@gmail.com>
2024-04-15 22:11:47 +0500

    add file first_preprocessed dataset

---
+++ lab4/titanic_preprocessed.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv("titanic_train.csv")
+
+mean_age = df['Age'].mean()
+
+df['Age'] = df['Age'].fillna(mean_age)
+
+df.to_csv("titanic_train_v2.csv", index=False)
